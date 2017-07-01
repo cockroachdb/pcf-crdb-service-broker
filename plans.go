0a59b89cc535cb42b98b02e5fa24a267c5a5f413
Radu Berinde <radu@cockroachlabs.com>
2017-07-01 08:54:04 -0400

    WIP

--- plans.go
+++ plans.go
@@ -35,9 +35,32 @@ type Plan struct {
 	CRDBHost      string `json:"crdbHost"`
 	CRDBPort      string `json:"crdbPort"`
 	CRDBAdminUser string `json:"crdbAdminUser"`
-	CRDBPassword  string `json:"crdbPassword"`
 
-	crdb *sql.DB
+	// SSLMode is one of:
+	//  - "disable": no SSL (for insecure cluster)
+	//  - "require": encrypted connection. Server verifies client's certificate.
+	//               Client does not verify server's certificate.
+	//  - "verify-ca": encrypted connection. Server verifies client's certificate.
+	//                 Client verifies server's certificate against the CA
+	//                 certificate, but does not verify that the host name matches
+	//                 its certificate.
+	//  - "verify-full": encrypted connection. Server verifies client's certificate.
+	//                   Client verifies server's certificate against the CA
+	//                   certificate and the host name matches that on the certificate.
+	// The recommended mode is "verify-full".
+	SSLMode string `json:"sslMode"`
+
+	// Client certificate (public key). Used for all modes except "disable".
+	SSLCert string `json:"sslCert"`
+	// Client private key. Used for all modes except "disable".
+	SSLKey string `json:"sslKey"`
+	// Certificate Authority certificate. Used for "verify-ca" and "verify-full".
+	SSLCACert string `json:"sslCACert"`
+
+	crdb        *sql.DB
+	sslCertFile string
+	sslKeyFile  string
+	sslCAFile   string
 }
 
 type Service struct {
@@ -111,12 +134,43 @@ func addPlan(p Plan) {
 	if p.CRDBHost == "" || p.CRDBPort == "" {
 		log.Fatal("init", fmt.Errorf("plan '%s' does not specify a CockroachDB host/port", p.Name))
 	}
+
 	if p.CRDBAdminUser == "" {
 		p.CRDBAdminUser = "root"
 	}
+
+	options := url.Values()
+	options.Add("sslmode", p.SSLMode)
+
+	switch p.SSLMode {
+	case "disable":
+	case "verify-ca", "verify-full":
+		var err error
+		p.sslCAFile, err = createTempFile("crdb-ssl-ca-", []byte(p.SSLCACert))
+		if err != nil {
+			log.Fatal("init-ca-file", err)
+		}
+		options.Add("sslrootcert", p.sslCAFile)
+		fallthrough
+	case "require":
+		var err error
+		p.sslCertFile, err = createTempFile("crdb-ssl-cert-", []byte(p.SSLCert))
+		if err != nil {
+			log.Fatal("init-cert-file", err)
+		}
+		options.Add("sslcert", p.sslCertFile)
+		p.sslKeyFile, err = createTempFile("crdb-ssl-key-", []byte(p.SSLKey))
+		if err != nil {
+			log.Fatal("init-key-file", err)
+		}
+		options.Add("sslkey", p.sslKeyFile)
+	default:
+		log.Fatal("init", fmt.Errorf("unknown ssl mode %s", p.SSLMode)
+	}
+
 	p.crdb, err = sql.Open(
 		"postgres",
-		dbURI(p.CRDBHost, p.CRDBPort, p.CRDBAdminUser, p.CRDBPassword, "" /* no database */),
+		dbURI(p.CRDBHost, p.CRDBPort, p.CRDBAdminUser, "" /* pass */, "" /* db */, options),
 	)
 	if err != nil {
 		log.Fatal("init-setup-db", err)
@@ -207,5 +261,17 @@ func InitServicesAndPlans() {
 	}
 	for _, p := range plans {
 		addPlan(p)
+	}
+}
+
+func CleanupPlans() {
+	for _, s := range Services {
+		for _, p := range s.Plans {
+			for _, file := range []string{p.sslCAFile, p.sslCertFile, p.sslKeyFile} {
+				if file != "" {
+					os.Remove(file)
+				}
+			}
+		}
 	}
 }
